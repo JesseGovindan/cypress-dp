ca54a694bb2e0baeb65f64679ca267da1c52c4b7
Nishka Maharaj <Nishka.Maharaj@chillisoft.co.za>
2022-10-21 15:09:30 +0200

    Request empty validation

--- cypress/e2e/empty_validation_request.cy.ts
+++ cypress/e2e/empty_validation_request.cy.ts
@@ -2,6 +2,10 @@ describe('empty spec', () => {
   it('passes', () => {
     const inputIdentifier = '[aria-label="Suggest a feature for our roadmap"]'
     cy.visit('http://localhost:3000')
-    cy.get(inputIdentifier).should('be.empty');
+    cy.get(inputIdentifier).clear();
+    cy.get('#request-button').click();
+    cy.get(`${inputIdentifier}:invalid`)
+    .invoke('prop', 'validationMessage')
+    .should('equal', 'Please fill out this field.')
   })
 })
