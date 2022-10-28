c381d86751d5865b332cd0c1b301f1a09f623fd6
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2022-10-28 14:20:04 +0200

    Subscribing with email address

---
+++ cypress/e2e/can_subscribe_with_email_address.cy.ts
@@ -0,0 +1,11 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+    const inputIdentifier = '[aria-label="Email for updates"]';
+    cy.get(inputIdentifier).type('njdlams@gmail.com');
+
+    cy.get('#subscribe').click();
+    cy.get(inputIdentifier).should('have.value', '');
+    cy.get('[role=status]').should('have.text', 'You are subscribed, stop joling around Nishka!');
+  })
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -175,7 +175,7 @@ export default function Roadmap({
       return toast.error(error)
     }
 
-    toast.success('You are now subscribed to feature updates!')
+    toast.success('You are subscribed, stop joling around Nishka!')
 
     if (subscribeInputRef.current) {
       subscribeInputRef.current.value = ''
@@ -253,6 +253,7 @@ export default function Roadmap({
                 className="px-3 py-3 mt-1 text-lg block w-full border border-gray-200 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring focus:ring-blue-300"
               />
               <button
+                id="subscribe"
                 className="flex items-center justify-center absolute right-2 top-2 px-4 h-10 border border-gray-200 text-gray-900 rounded-md w-14 focus:outline-none focus:ring focus:ring-blue-300 focus:bg-gray-100"
                 type="submit"
               >
